d15e793d664560fa522c7404996466113f3849a7
Julien Lemaitre <julien.lemaitre@clarius.com>
2022-12-01 15:00:40 -0800

    Update Android example
    - fixed "cannot connect" issue when trying to connect to the probe's Wi-Fi after auto-joining: need to pass the network ID to the cast connect function to let cast bind the TCP socket to the network
    - added raw data example
    - moved cast code into separate service

--- android/src/examples/cast_android/README.md
+++ android/src/examples/cast_android/README.md
@@ -1,9 +1,42 @@
-Clarius Cast Example
-====================
+# Clarius Cast Example
 
 1. Copy the `aar` package into the `libs` subfolder
 2. Re-sync Gradle
 3. Build
 
-TODO
+## TODO
+
 * Provide a multi-ABI package
+
+## Marketplace Integration
+
+### Launcher
+
+The Clarius app can launch partner apps with an Android intent containing the probe connection info in the intent's extras:
+
+| Key | Description | Type |
+|-----|-------------|------|
+| `cus_probe_serial` | probe serial | UTF-8 string in a `byte[]` |
+| `cus_ip_address` | probe's IP address | UTF-8 string in a `byte[]` |
+| `cus_cast_port` | probe's TCP port | Java `int` |
+
+### Visibility
+
+The Clarius app attempts to detected installed partner apps by querying the `PackageManager` to adapt its GUI.
+However, starting with Android 11 (API 30), results are filtered by the system, preventing detection.
+To allow detection, ensure your target activity declares an intent filter in the manifest file with either of the following actions:
+
+* `android.intent.action.MAIN`
+* `me.clarius.sdk.action.LAUNCH`
+
+For example:
+
+    <application>
+        <activity>
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <action android:name="me.clarius.sdk.action.LAUNCH" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
+    </application>
--- android/src/examples/cast_android/app/src/main/java/me/clarius/sdk/cast/example/FirstFragment.java
+++ android/src/examples/cast_android/app/src/main/java/me/clarius/sdk/cast/example/FirstFragment.java
@@ -1,8 +1,13 @@
 package me.clarius.sdk.cast.example;
 
-import android.graphics.Bitmap;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,24 +16,64 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.fragment.NavHostFragment;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.function.Function;
 
+import me.clarius.sdk.Cast;
+import me.clarius.sdk.ProbeInfo;
 import me.clarius.sdk.UserFunction;
 import me.clarius.sdk.cast.example.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
     private static final String TAG = "Cast";
-    private final ExecutorService executorService = Executors.newFixedThreadPool(1);
 
     private FragmentFirstBinding binding;
-    private CastWrapper wrapper;
-    private CastViewModel viewModel;
+    private CastService.CastBinder castBinder;
+    private Optional<Long> networkID = Optional.empty();
+
+    /**
+     * Defines callbacks for service binding, passed to bindService()
+     */
+    private final ServiceConnection castConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            Log.d(TAG, "service connected");
+            // We've bound to our service, cast the IBinder now
+            castBinder = (CastService.CastBinder) service;
+            castBinder.getProcessedImage().observe(getViewLifecycleOwner(), binding.imageView::setImageBitmap);
+            castBinder.getError().observe(getViewLifecycleOwner(), FirstFragment.this::showError);
+            castBinder.getRawDataProgress().observe(getViewLifecycleOwner(), binding.rawDataDownloadProgressBar::setProgress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName component) {
+            castBinder = null;
+        }
+    };
+
+    private static String fromByteArray(final byte[] from) {
+        return new String(from, StandardCharsets.UTF_8);
+    }
+
+    private static void askProbeInfo(Cast cast) {
+        cast.getProbeInfo(info -> Log.d(TAG, "Probe info: " + info.map(FirstFragment::probeInfoToString).orElse("<none>")));
+    }
+
+    public static String probeInfoToString(final ProbeInfo info) {
+        StringBuilder b = new StringBuilder();
+        b.append("v").append(info.version).append(" elements: ").append(info.elements)
+                .append(" pitch: ").append(info.pitch)
+                .append(" radius: ").append(info.radius);
+        return b.toString();
+    }
 
     @Override
     public View onCreateView(
@@ -39,50 +84,79 @@ public class FirstFragment extends Fragment {
         return binding.getRoot();
     }
 
+    private static <T> Optional<T> maybeInteger(final byte[] from, Function<String, T> transform) {
+        try {
+            final String fromString = new String(from);
+            final T ret = transform.apply(fromString);
+            return Optional.of(ret);
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        viewModel = new ViewModelProvider(this).get(CastViewModel.class);
+        final Intent intent = this.requireActivity().getIntent();
+        if (intent != null) {
+            final Bundle extras = intent.getExtras();
+            if (extras != null) {
+                networkID = maybeInteger(extras.getByteArray("cus_network_id"), Long::parseLong);
+                Log.d(TAG, "Received network ID: " + networkID.orElse(-1L));
+                final Optional<String> probeSerial = Optional.ofNullable(extras.getByteArray("cus_probe_serial")).map(FirstFragment::fromByteArray);
+                final Optional<String> ipAddress = Optional.ofNullable(extras.getByteArray("cus_ip_address")).map(FirstFragment::fromByteArray);
+                final Optional<String> castPort = Optional.ofNullable(extras.getByteArray("cus_cast_port")).map(FirstFragment::fromByteArray);
+                Log.d(TAG, "Received probe serial: " + probeSerial.orElse("<none>"));
+                Log.d(TAG, "Received IP address: " + ipAddress.orElse("<none>"));
+                Log.d(TAG, "Received cast port: " + castPort.orElse("<none>"));
+                if (ipAddress.isPresent()) {
+                    binding.ipAddress.setText(ipAddress.get());
+                }
+                if (castPort.isPresent()) {
+                    binding.tcpPort.setText(castPort.get());
+                }
+            }
+        }
 
-        wrapper = new CastWrapper(requireContext(), executorService, viewModel);
-
-        viewModel.getProcessedImage().observe(getViewLifecycleOwner(), binding.imageView::setImageBitmap);
-        viewModel.getError().observe(getViewLifecycleOwner(), this::showError);
-
-        binding.buttonBluetooth.setOnClickListener(v -> NavHostFragment.findNavController(FirstFragment.this)
-                .navigate(R.id.action_FirstFragment_to_SecondFragment));
         binding.buttonConnect.setOnClickListener(v -> doConnect());
         binding.buttonRun.setOnClickListener(v -> toggleRun());
-        binding.buttonAskState.setOnClickListener(v -> wrapper.askState());
         binding.buttonDisconnect.setOnClickListener(v -> doDisconnect());
+        binding.buttonRawData.setOnClickListener(v -> getRawData());
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Context context = requireContext();
+        Intent intent = new Intent(context, CastService.class);
+        context.bindService(intent, castConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Context context = requireContext();
+        context.unbindService(castConnection);
+        castBinder = null;
     }
 
     private void toggleRun() {
-        if (wrapper == null) {
+        if (castBinder == null) {
             showError("Clarius Cast not initialized");
             return;
         }
-        Log.d(TAG, "run freeze");
-        wrapper.getCast().userFunction(UserFunction.Freeze, 0, UserFunction.Freeze);
+        showMessage("Toggle run");
+        castBinder.getCast().userFunction(UserFunction.Freeze, 0, result -> Log.d(TAG, "Freeze function result: " + result));
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        if (wrapper != null) {
-            wrapper.getCast().disconnect();
-            wrapper.getCast().release();
-            wrapper = null;
-        }
         binding = null;
     }
 
-    private void updateImage(Bitmap bitmap) {
-        binding.imageView.setImageBitmap(bitmap);
-    }
-
     private void doConnect() {
-        if (wrapper == null) {
+        if (castBinder == null) {
             showError("Clarius Cast not initialized");
             return;
         }
@@ -100,15 +174,33 @@ public class FirstFragment extends Fragment {
             binding.tcpPortLayout.setError("Invalid number");
             return;
         }
-        Log.d(TAG, "Connecting to " + ipAddress + ":" + tcpPort);
-        wrapper.getCast().connect(ipAddress, tcpPort, getCertificate());
+        showMessage("Connecting to " + ipAddress + ":" + tcpPort);
+        castBinder.getCast().connect(ipAddress, tcpPort, networkID, getCertificate(),
+                result -> {
+                    Log.d(TAG, "Connection result: " + result);
+                    if (result) {
+                        askProbeInfo(castBinder.getCast());
+                    }
+                });
     }
 
     private void doDisconnect() {
-        if (wrapper == null) {
+        if (castBinder == null) {
             return;
         }
-        wrapper.getCast().disconnect();
+        castBinder.getCast().disconnect(result -> Log.d(TAG, "Disconnection result: " + result));
+    }
+
+    private void getRawData() {
+        if (castBinder == null) {
+            return;
+        }
+        showMessage("Requesting raw data");
+        binding.rawDataDownloadProgressBar.setProgress(0);
+        binding.rawDataCopyProgressBar.setProgress(0);
+        final Cast cast = castBinder.getCast();
+        RawDataFile handle = new RawDataFile(cast);
+        cast.requestRawData(0, 0, handle::requestResultRetrieved);
     }
 
     private void showError(CharSequence text) {
@@ -117,7 +209,50 @@ public class FirstFragment extends Fragment {
         mainHandler.post(() -> Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show());
     }
 
+    private void showMessage(CharSequence text) {
+        Log.d(TAG, (String) text);
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        mainHandler.post(() -> Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show());
+    }
+
     private String getCertificate() {
         return "research";
     }
+
+    private class RawDataFile {
+        final Cast cast;
+
+        RawDataFile(Cast cast) {
+            this.cast = cast;
+        }
+
+        void requestResultRetrieved(int result) {
+            if (result > 0) {
+                showMessage("Found raw data");
+                cast.readRawData(this::rawDataRetrieved);
+            } else {
+                showMessage("Failed to request raw data, ensure raw data buffering is enabled and image is frozen");
+            }
+        }
+
+        void rawDataRetrieved(int result, ByteBuffer data) {
+            if (result > 0) {
+                try {
+                    showMessage("Saving raw data");
+                    Uri uri = IOUtils.saveInDownloads(data, requireContext(),
+                            (progress, total) -> {
+                                binding.rawDataCopyProgressBar.setMax(total);
+                                binding.rawDataCopyProgressBar.setProgress(progress);
+                            });
+                    showMessage("Saved raw data in file " + uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    showError(e.toString());
+                }
+            } else {
+                showError("Could not retrieve raw data");
+            }
+        }
+    }
+
 }
