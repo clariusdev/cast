3510c1f530a4930e68d48c6286fcaa7e21dfacff
Ali Uneri <auneri@users.noreply.github.com>
2025-06-03 13:49:55 -0400

    Add MSVC compatibility to cast_export.h
    Modified cast_export.h to support MSVC by conditionally replacing GCC-specific __attribute__ usage with __declspec.

--- include/cast/cast_export.h
+++ include/cast/cast_export.h
@@ -2,6 +2,18 @@
 #ifndef CAST_EXPORT_H
 #define CAST_EXPORT_H
 
+#ifdef _MSC_VER
+#  define CAST_HELPER_DLL_IMPORT __declspec(dllimport)
+#  define CAST_HELPER_DLL_EXPORT __declspec(dllexport)
+#  define CAST_HELPER_DLL_LOCAL
+#  define CAST_DEPRECATED __declspec(deprecated)
+#else
+#  define CAST_HELPER_DLL_IMPORT __attribute__((visibility("default")))
+#  define CAST_HELPER_DLL_EXPORT __attribute__((visibility("default")))
+#  define CAST_HELPER_DLL_LOCAL  __attribute__((visibility("hidden")))
+#  define CAST_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
 #ifdef CAST_STATIC_DEFINE
 #  define CAST_EXPORT
 #  define CAST_NO_EXPORT
@@ -9,20 +21,16 @@
 #  ifndef CAST_EXPORT
 #    ifdef cast_EXPORTS
         /* We are building this library */
-#      define CAST_EXPORT __attribute__((visibility("default")))
+#      define CAST_EXPORT CAST_HELPER_DLL_EXPORT
 #    else
         /* We are using this library */
-#      define CAST_EXPORT __attribute__((visibility("default")))
+#      define CAST_EXPORT CAST_HELPER_DLL_IMPORT
 #    endif
 #  endif
 
 #  ifndef CAST_NO_EXPORT
-#    define CAST_NO_EXPORT __attribute__((visibility("hidden")))
+#    define CAST_NO_EXPORT CAST_HELPER_DLL_LOCAL
 #  endif
-#endif
-
-#ifndef CAST_DEPRECATED
-#  define CAST_DEPRECATED __attribute__ ((__deprecated__))
 #endif
 
 #ifndef CAST_DEPRECATED_EXPORT
